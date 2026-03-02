e2b4f23341c24bd3726ed6514b39e5dccfba2ccf
moises <ghmoises97@gmail.com>
2026-03-02 18:42:26 -0500

    perdonar estoy probando algo

--- autenticacion.py
+++ autenticacion.py
@@ -1,18 +1,5 @@
+print ("=======================")
+print ("=TechBank Riwi digital=")
+print ("=======================")
 usuario_autenticacion = "David"
-
-usuario =input ("ingrese usuario: ")
-
-
-def control_intentos():
-    usuario_autenticacion = "David"
-
-    for i in range(3):
-        pin = input("Ingrese PIN: ")
-        
-        if pin.lower() == usuario_autenticacion.lower():
-            print("PIN Correcto.")
-            break
-        else:
-            print(f"Error, PIN incorrecto. Intento: {i + 1}")
-    else:
-        print("No tiene más intentos.")
+usuario = input ("ingrese usuario: ")
