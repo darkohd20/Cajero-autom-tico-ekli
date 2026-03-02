49fc18fadec2bb3f2ca9f6889e521af6dd4219cc
moises <ghmoises97@gmail.com>
2026-03-02 18:33:41 -0500

    correccion definitiva

--- autenticacion.py
+++ autenticacion.py
@@ -1,19 +1,18 @@
-usuario_autenticacion = "david" 
-intentos = 3
-while intentos > 0:
-    usaurio = input ("ingrese usuario: ")
-    
-    if usaurio == usuario_autenticacion: 
-        print ("USUARIO CORRECTO.")
-        print ("=======================")
-        print ("=TechBank Riwi digital=")
-        print ("====Bienvenido David===")
-        print ("=======================")
-        break
-    else: 
-        intentos-=1
-        print (f"usuario no encontrado.")
-    
-if intentos==0:
-    print ("USUARIO BLOQUEADO LLAME A LINEA DE ATENCION: 3214567890")
-    
+usuario_autenticacion = "David"
+
+usuario =input ("ingrese usuario: ")
+
+
+def control_intentos():
+    usuario_autenticacion = "David"
+
+    for i in range(3):
+        pin = input("Ingrese PIN: ")
+        
+        if pin.lower() == usuario_autenticacion.lower():
+            print("PIN Correcto.")
+            break
+        else:
+            print(f"Error, PIN incorrecto. Intento: {i + 1}")
+    else:
+        print("No tiene más intentos.")
