65ad0768ef98f8897d19c7ea9a3341d65e096997
Moises Alfredo Gutiérrez Haad <ghmoises97@gmail.com>
2026-03-02 10:57:18 -0500

    proceso

--- autenticacion.py
+++ autenticacion.py
@@ -1,1 +1,11 @@
-a
+usuario = "David" 
+contrasena = "145D"
+intentos = 3 
+
+
+
+while intentos > 0:
+    usuario: input ("ingrese usuario: ")
+    contrasena: input ("ingrese contraseña: ")
+    
+    if usuario == "David" and contrasena == "145D":
