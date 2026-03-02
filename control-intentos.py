fdf3670592f97e0ce8fc620fb9bdbc335d025b29
Camila Marrugo <marrugocamille@gmail.com>
2026-03-02 18:20:18 -0500

    Control de Intentos V1

---
+++ control-intentos.py
@@ -0,0 +1,13 @@
+def control_intentos():
+    usuario_autenticacion = "David"
+
+    for i in range(3):
+        pin = input("Ingrese PIN: ")
+        
+        if pin.lower() == usuario_autenticacion.lower():
+            print("PIN Correcto.")
+            break
+        else:
+            print(f"Error, PIN incorrecto. Intento: {i + 1}")
+    else:
+        print("No tiene más intentos.")
