8d58bdbca57a69ca6cecc097f67b03334482dbac
ronaldo <rrodriguezd@unibarranquilla.edu.co>
2026-03-02 09:55:39 -0500

    config 4

--- conf.py
+++ conf.py
@@ -3,4 +3,7 @@ from datetime import datetime
 saldo= 1000
 n = input("Ingrese su nombre: ")
 os.system("clear")
-print("      BANCO PYTHON INTERNACIONAL   ")
+print("     TechBank Riwi Digital    ")
+print("Fecha:", datetime.now().strftime("%I:%M:%S %p"))
+print("Hola", n)
+print("¿Qué deseas hacer hoy?")         
