55ea807a6da78e802790d335710d0ea9349c7b31
4ndr3s-00 <andresteheranr@gmail.com>
2026-03-03 07:25:50 -0500

    feat(ñekli): add second part of interactive menu implementation

--- Menu.py
+++ Menu.py
@@ -4,3 +4,16 @@ def menu():
     print("2. Retirar")
     print("3. Consultar saldo")
     print("4. Salir")
+
+while True:
+    try:
+        operaciones = int(input("Cuántas operaciones desea realizar?: "))
+        
+
+        if operaciones <= 0:
+            print("Debe ingresar un número válido")
+        else:
+            break
+
+    except ValueError:
+        print("Eso no es un número, inténtelo otra vez")
