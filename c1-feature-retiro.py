8f408ce026ad440fa0f79c2af9e0c7f1e8594cec
onomauri <onofremauro0@gmail.com>
2026-03-03 07:30:17 -0500

    v3

--- c1-feature-retiro.py
+++ c1-feature-retiro.py
@@ -1,8 +1,16 @@
 saldo = 1000
 
-menu = int(input("Opcion de retiro ingrse 2: "))
+try:
+    menu = int(input("Opcion de retiro ingrese 2: "))
 
-if menu == 2:
-    monto = float(input("ingrese valor a retirar: "))
-    saldo = saldo - monto
-    print(f"Operacion exitosa su salgo actual es {saldo}.")
+    if menu == 2:
+        monto = float(input("ingrese valor a retirar: "))
+        saldo = saldo - monto
+        if  monto <= 0:
+            print("Valor invalido")
+        elif monto > saldo:
+            print("Fondos insuficientes.")
+        else:
+            print(f"Operacion exitosa su salgo actual es {saldo}.")
+except:
+    print("Error:valor invalido, ingrese una de las opcines disponible")
