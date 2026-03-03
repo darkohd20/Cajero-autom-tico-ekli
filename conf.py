0c4e021fb5d8219233ad9e78be73c2b2dd551c54
RONALDO RODRIGUEZ DE LIMA <165290429+MOKEXD@users.noreply.github.com>
2026-03-03 07:22:42 -0500

    Clean up conf.py by removing prints and clear command
    Removed unnecessary print statements and clear command.

--- conf.py
+++ conf.py
@@ -2,7 +2,5 @@ import os
 from datetime import datetime   
 saldo= 1000
 n = input("Ingrese su nombre: ")
-os.system("clear")
-print("     TechBank Riwi Digital    ")
-print("Fecha:", datetime.now().strftime("%I:%M:%S %p"))
+
 
