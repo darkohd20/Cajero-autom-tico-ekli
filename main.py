454be44738b07f0491d0986da3a1bd243d14c100
darkohd20 <cristiandelahoz2028@gmail.com>
2026-03-03 07:39:46 -0500

    subida de code conf a main

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import os
 from datetime import datetime   
 saldo= 1000
 n = input("Ingrese su nombre: ")
-os.system("clear")
-print("     TechBank Riwi Digital    ")
-print("Fecha:", datetime.now().strftime("%I:%M:%S %p"))
-print("Hola", n)
-print("¿Qué deseas hacer hoy?")         
+
+
