fb1080a3d37882d3f9fad089d62a0d19b8bc13a9
onomauri <onofremauro0@gmail.com>
2026-03-03 07:01:12 -0500

    v2

--- c1-feature-retiro.py
+++ c1-feature-retiro.py
@@ -1,1 +1,8 @@
- 
+saldo = 1000
+
+menu = int(input("Opcion de retiro ingrse 2: "))
+
+if menu == 2:
+    monto = float(input("ingrese valor a retirar: "))
+    saldo = saldo - monto
+    print(f"Operacion exitosa su salgo actual es {saldo}.")
