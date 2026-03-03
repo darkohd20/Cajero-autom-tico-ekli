93e67e1495ab2b9c1ca6d6d436f097d925a7344c
Moises Alfredo Gutiérrez Haad <ghmoises97@gmail.com>
2026-03-03 07:29:45 -0500

    Update autenticacion.py

--- autenticacion.py
+++ autenticacion.py
@@ -1,5 +1,7 @@
+print("Fecha:", datetime.now().strftime("%I:%M:%S %p"))
 print ("=======================")
 print ("=TechBank Riwi digital=")
 print ("=======================")
+print ("bienvenido", n, "Por favor ingrese el pin: ")
 usuario_autenticacion = "David"
-usuario = input ("ingrese usuario: ")
+usuario = input ("Pin: ")
