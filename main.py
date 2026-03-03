656aab441e05a509ad0aaac86d1fd3798b28e534
darkohd20 <cristiandelahoz2028@gmail.com>
2026-03-02 19:44:32 -0500

    mover  el code de conf.py a la rama main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-saldo = 1000
+import os
+from datetime import datetime   
+saldo= 1000
+n = input("Ingrese su nombre: ")
+os.system("clear")
+print("     TechBank Riwi Digital    ")
+print("Fecha:", datetime.now().strftime("%I:%M:%S %p"))
+print("Hola", n)
+print("¿Qué deseas hacer hoy?")         
